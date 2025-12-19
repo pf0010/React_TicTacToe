8aa8299ed2cf54b1e1fc0c6c267ab88842fccd60
Patrick Fitzstephens <pfitzste@ucsc.edu>
2025-12-18 17:09:03 -0800

    Squares are now rendered with a nested loop

--- src/App.js
+++ src/App.js
@@ -28,24 +28,25 @@ function Board({ xIsNext, squares, onPlay }) {
         status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
+    const renderSquares = () => Array.from({ length: 3 }).map((v, i) => {
+        let s;
+        let row = [];
+        for (let j = 0; j < 3; ++j) {
+            s = i ? i * j : j;
+            row.push(<Square value={squares[s]} onSquareClick={() => handleClick(s)} />);
+        }
+
+        return (
+            <div className="board-row">{row}</div>
+        );
+    });
+
+
+
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            <div>{renderSquares()}</div>
         </>
     );
 }
@@ -85,8 +86,6 @@ export default function Game() {
             );
         }
     });
-
-    console.log(moves);
 
     return (
         <div className="game">
